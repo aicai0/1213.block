5f4b7cebb9ffa830d470b48b8bcb656e1f6070fc
aicai0 <18335763671@163.com>
2017-12-14 00:20:56 +0800

    1213.block

---
+++ js/blocktp.js
@@ -0,0 +1,14 @@
+$(function(){
+  /*  myScroll = new IScroll('#wrapper', {
+        scrollX: true,
+        scrollY: false,
+        mouseWheel: true,
+        click:true
+    });
+    var swiper = new Swiper('.swiper-container', {
+        pagination: {
+            el: '.swiper-pagination',
+            dynamicBullets: true,
+        },
+    });*/
+});
